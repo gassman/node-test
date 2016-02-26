8e9f7f882bd4cbdd8dad6ce782ae4b684d4bfb31
Yon <yon@golanfamily.com>
2016-02-26 17:12:07 +1100

    comment out auth route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var auth = require('./routes/auth');
+//var auth = require('./routes/auth');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/auth',auth);
+//app.use('/auth',auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
